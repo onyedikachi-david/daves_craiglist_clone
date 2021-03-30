1e6427e58fb3556e0fde6ad5dca4a71387091ab2
David Anyatonwu <davidanyatonwu@gmail.com>
2021-03-30 21:45:57 +0100

    edited settins.py

--- craigelist_app/settings.py
+++ craigelist_app/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
+from decouple import config
 # import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
