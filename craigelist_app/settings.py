36cdb58183acc69bd1d76ca8d92e379f4927cdf1
David Anyatonwu <davidanyatonwu@gmail.com>
2021-03-30 22:20:33 +0100

    edited hjghj

--- craigelist_app/settings.py
+++ craigelist_app/settings.py
@@ -28,7 +28,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
